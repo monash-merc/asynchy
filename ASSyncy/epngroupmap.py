9a3b31f483a99bf782b8009da01aef0a6a119972
Chris Hines <chris.hines@monash.edu>
2018-02-16 13:31:39 +1100

    new program to alert us to epns that we are authorised to but aren't mapped to any existing groups

---
+++ ASSyncy/epngroupmap.py
@@ -0,0 +1,69 @@
+from . import ASPortal
+from . import ASTransfer
+from . import ASSync
+import dateutil
+import dateutil.tz
+import threading
+
+
+
+class Epngroupmap(object):
+
+    def __init__(self,config):
+        import yaml
+        self.config={}
+        with open(config) as f:
+            self.config=yaml.load(f.read())
+        self.tz=dateutil.tz.gettz('Australia/Melbourne') # This is the timezone for this script. It actually doesn't matter what value is used here as calls to datetime.datetime.now(self.tz) will convert to whatever timezone you specify and comparions just need a TZ in both sides of the operator
+        self.assync = ASSync(config)
+
+
+
+    def main(self):
+        import requests
+        import datetime
+        unknownepns = {}
+        epns = ASTransfer.transfermethod.list(self.assync.getTransferParams(None),None)
+        #print(epns)
+        unknownepns = {}
+        for e in epns:
+            if e == b'.':
+                continue
+            m3cap = self.assync.get_m3cap(e.decode())
+            if m3cap == None:
+                unknownepns[e.decode()] = None
+        print("unknown epns {}".format(unknownepns))
+        equipment=[]
+        s = requests.Session()
+        for e in self.config['equipment']:
+            equipment.append( ASPortal.Connection(s,e['username'],e['password'],equipmentID="{}".format(e['id'])))
+        for e in equipment:
+            e.auth()
+
+        now=datetime.datetime.now()
+        visits=[]
+        for e in equipment:
+            visits.extend(e.getVisits(start_time=now-datetime.timedelta(days=200),end_time=now))
+        for v in visits:
+            if v['epn'] in unknownepns:
+                unknownepns[v['epn']] = v
+        for (epn,visit) in unknownepns.items():
+            print("epn {} is not known, it includes scientists {} on {}. Please add it the config".format(epn,visit['data_scientists'],visit['start_time']))
+
+        
+
+        
+
+def main():
+    import sys
+    if len(sys.argv)>1:
+        config=sys.argv[1]
+    else:
+        print("Usage: {} <config.yml>".format(sys.argv[0]))
+        exit(1)
+    sync=Epngroupmap(config)
+    sync.main()
+
+if __name__ == '__main__':
+    main()
+
