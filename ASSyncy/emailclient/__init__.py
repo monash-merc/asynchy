77bb984b3197c5e3b9da20d2b5583a526f3a7a04
Chris Hines <chris.hines@monash.edu>
2018-02-16 14:20:26 +1100

    add emailclient module

---
+++ ASSyncy/emailclient/__init__.py
@@ -0,0 +1,22 @@
+class EmailClient():
+
+    def send(self, tmplsrc, msgvars, debug=True):
+        """If debug is set to true, don't actually send the email"""
+
+        import logging
+        logger=logging.getLogger()
+        import jinja2
+        results = []
+        template = jinja2.Template(tmplsrc)
+        for recipient in msgvars['to']:
+            thismsg = msgvars.copy()
+            thismsg['to'] = recipient
+            thismsg['from'] = 'help@massive.org.au'
+            rendered = template.render(**thismsg)
+            results.append(rendered)
+            logger.info("Sending notification to {}".format(recipient))
+            if not debug:
+                s = smtplib.SMTP('smtp.monash.edu')
+                s.sendmail(thismsg['from'], thismsg['to'], rendered)
+                print(rendered)
+        return results
